1f113157cb9d02b0c087d9a0bc3c187069084dc6
brunovazzf <brunovazzf@gmail.com>
2026-04-26 16:42:21 -0300

    Update top.v

--- top.v
+++ top.v
@@ -1,12 +1,20 @@
 module top (
-    input [31:0] in, 
-    input [1:0] sel, 
-    output [7:0] out);
+    input  [7:0] a, b, c, d, // As 4 entradas de 1 byte
+    input  [1:0] sel,        // Seletor de 2 bits
+    output [7:0] out         // Saída de 1 byte
+);
+    wire [7:0] w_ab, w_cd;
+    
+    mux2byte mux_semi1 (a, b, sel[0], w_ab);
+    mux2byte mux_semi2 (c, d, sel[0], w_cd);
+    mux2byte mux_final (w_ab, w_cd, sel[1], out);
 
-    //declare os fios intermediarios 
+endmodule
 
-    // instancie os muxes que precisar 
-
-
-
+module mux2byte (
+    input  [7:0] a, b, 
+    input        sel, 
+    output [7:0] out
+);
+    assign out = sel ? b : a;
 endmodule
